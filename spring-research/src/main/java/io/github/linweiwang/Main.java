ce5ac22e9327f5901623908db1e9bab278d575c8
linweiwang <linweiwang@hotmail.com>
2024-04-09 23:04:34 +0800

    01. 环境搭建.

---
+++ spring-research/src/main/java/io/github/linweiwang/Main.java
@@ -0,0 +1,13 @@
+package io.github.linweiwang;
+
+import io.github.linweiwang.bean.Person;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		Person person = context.getBean(Person.class);
+		System.out.println(person);
+	}
+}
